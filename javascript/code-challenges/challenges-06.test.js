ba10d3ac55d5891cbc6542763dadb458752586b7
woodwardem <woodwardem@g.crookcountyschools.org>
2022-01-16 20:05:29 -0800

    test challenges 4 and 5 need help

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -92,7 +92,10 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
+  isAuthor() = 
+people + isAuthor;
+  isAuthor(true);
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -161,7 +164,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
